32d1ff6f1c32ce738b332608ef76ad82bdc1d783
xingyada <xingyada@126.com>
2018-12-07 16:54:38 +0800

    iconfont

---
+++ Travel/src/pages/home/components/Header.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="header">
+        <div class="header-left">
+            <div class="icon iconfont icon-fanhui"></div>
+        </div>
+        <div class="header-input">
+            <span class="icon iconfont icon-sousuo"></span>
+            输入城市/景点/游玩主题
+        </div>
+        <div class="header-right">
+            城市
+            <span class="icon iconfont icon-xiangxia"> </span>
+        </div>
+    </div>
+</template>
+
+<script>    
+export default {
+    name : 'HomeHeader'
+}
+</script>
+
+<style lang="stylus" scoped>
+    @import '~styles/variables.styl'
+    .header
+      display:flex
+      line-height: 0.86rem
+      background: $bgColor
+      color:#fff
+      .header-left
+        width: .64rem
+        float: left
+        .icon
+            text-align:center
+            font-size: .4rem
+      .header-input
+        flex:1
+        background:#fff
+        border-radius: .1rem
+        line-height: .64rem
+        height: .64rem
+        margin-top: .12rem
+        margin-left:.2rem
+        color:#ccc
+      .header-right
+        width:1.24rem
+        float:right
+        text-align:center
+        .icon
+            margin-left: -.04rem
+            font-size: .24rem
+</style>
--- Travel/src/router/index.js
+++ Travel/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-
+import Home from '@/pages/home/Home.vue'
+import List from '@/pages/list/List.vue'
 
 Vue.use(Router)
 
@@ -8,6 +9,13 @@ export default new Router({
   routes: [
     {
       path: '/',
+      name :'Home',
+      component :Home
+    },
+    {
+      path:'/list',
+      name:'List',
+      component:List
     }
   ]
 })
