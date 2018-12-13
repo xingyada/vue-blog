685deb2e9847b37089a86253aac380143d7928b3
xingyada <xingyada@126.com>
2018-12-13 18:55:30 +0800

    添加Recommend 和 Weekend组件

---
+++ Travel/src/pages/home/components/Recommend.vue
@@ -0,0 +1,77 @@
+<template>
+    <div>
+        <div class="title">热销推荐</div>
+        <ul>
+            <li class="item border-bottom" v-for="list in recommendList" :key = "list.id">
+                <img class="item-img" :src="list.imgUrl" />
+                <div class="item-info">
+                    <p class="item-title">{{list.title}}</p>
+                    <p class="item-desc">{{list.desc}}</p>
+                    <button class="item-button">查看详情</button>
+                </div>
+            </li>
+        </ul>
+    </div>
+</template>    
+<script>
+    export default {
+        name : 'HomeRecommend',
+        data (){
+            return {
+                recommendList : [{
+                    id: 'recom001',
+                    imgUrl: 'http://imgs.qunarzz.com/ftejia/1806/f9/8a23740f5a6f86.jpg',
+                    title: '地名',
+                    desc: '一个简单的介绍，总部设在驻马店，十一人民九个骗啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊'
+                }, {
+                    id: 'recom002',
+                    imgUrl: 'http://img1.qunarzz.com/sight/p0/1409/19/adca619faaab0898245dc4ec482b5722.jpg_200x200_1bc99086.jpg',
+                    title: '故宫',
+                    desc: '一个简单的介绍，总部设在驻马店，十一人民九个骗啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊'
+                }, {
+                    id: 'recom003',
+                    imgUrl: 'http://img1.qunarzz.com/sight/p0/1501/40/40b2b6c951b28fdd.water.jpg_250x250_ecfa0df2.jpg',
+                    title: '水立方',
+                    desc: '一个简单的介绍，总部设在驻马店，十一人民九个骗啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊'
+                }]
+            }
+        }
+    }
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/mixins.styl'
+
+.title
+    margin-top: .2rem
+    line-height: .8rem
+    background : #eee
+    text-indent: .2rem
+.item
+    overflow:hidden
+    display:flex
+    height:1.9rem
+    .item-img
+        width:1.7rem
+        height:1.7rem
+        padding: .1rem
+    .item-info
+        flex: 1
+        min-width: 0
+        padding: .1rem
+       .item-title
+            line-height: .54rem
+            font-size: .32rem
+            ellipsis()
+      .item-desc
+            line-height: .4rem
+            color: #ccc
+            ellipsis()
+      .item-button
+            line-height: .44rem
+            margin-top: .16rem
+            background: #ff9300
+            padding: 0 .1rem
+            border-radius: .06rem
+            color: #fff
+</style>
---
+++ Travel/src/pages/home/components/Weekend.vue
@@ -0,0 +1,63 @@
+<template>
+    <div>
+        <div class="title">周末游</div>
+        <ul>
+            <li class="item border-bottom" v-for="item of recommendList" :key="item.id">
+                <div class="item-img-wrapper">
+                    <img class="item-img" :src='item.imgUrl' />
+                </div>
+                <div class="item-info">
+                    <p class="item-title">{{item.title}}</p>
+                    <p class="item-desc">{{item.desc}}</p>
+                </div>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'HomeWeekend',
+        data: function () {
+            return {
+                recommendList: [{
+                    id: '0001',
+                    imgUrl: 'https://dimg07.c-ctrip.com/images/w30i0o000000enulf6C20.jpg_Z_800_600.jpg',
+                    title: '地名1',
+                    desc: '地名的一个简单介绍sdkbksksjbksbksdjhskksjbkbvdkbk啊啊啊啊啊啊啊啊'
+                }, {
+                    id: '0002',
+                    imgUrl: 'https://dimg07.c-ctrip.com/images/w30i0o000000enulf6C20.jpg_Z_800_600.jpg',
+                    title: '地名2',
+                    desc: '地名的一个简单介绍sdkbksksjbksbksdjhskksjbkbvdkbk啊啊啊啊啊啊啊啊'
+                }]
+            }
+        }
+    }
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/mixins.styl'
+
+  .title
+    margin-top: .2rem
+    line-height: .8rem
+    background: #eee
+    text-indent: .2rem
+  .item-img-wrapper
+    overflow: hidden
+    height: 0
+    padding-bottom: 60%
+    .item-img
+      width: 100%
+  .item-info
+    padding: .1rem
+    .item-title
+      line-height: .54rem
+      font-size: .32rem
+      ellipsis()
+    .item-desc
+      line-height: .4rem
+      color: #ccc
+      ellipsis()
+</style>
