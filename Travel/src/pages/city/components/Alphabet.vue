1d3ac6be9e8ca98c8cf0a16f9e4f90a8b3811c90
xingyada <xingyada@126.com>
2018-12-15 17:41:01 +0800

    add List and Alphabet.vue

---
+++ Travel/src/pages/city/components/Alphabet.vue
@@ -0,0 +1,21 @@
+<template>
+    <ul class="list">
+        <li class="item">A</li>
+        <li class="item">A</li>
+        <li class="item">A</li>
+        <li class="item">A</li>
+        <li class="item">A</li>
+        <li class="item">A</li>
+    </ul>
+</template>
+
+<script>
+    import Bscroll from 'better-scroll'
+    export default {
+        name: 'CityAlphabet'
+    }
+</script>
+
+<style lang="stylus" scoped>
+    @import '~styles/variables.styl'
+</style>
